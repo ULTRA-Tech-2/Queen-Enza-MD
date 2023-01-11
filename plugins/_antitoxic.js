84ae39f122228980c550053660f064ac5796013a
Mr Juice <chigonyetawanda@gmail.com>
2023-01-11 10:41:12 +0500

    Add files via upload

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,4 +1,4 @@
-const toxicRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|asu|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|goblok|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i
+const toxicRegex = /Fuck|lol|puto|puta|rata|estupido|imbecil|rctmre|mrd|verga|vrga|maricon/i
 
 export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     if (m.isBaileys && m.fromMe)
@@ -11,14 +11,14 @@ export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     const isToxic = toxicRegex.exec(m.text)
     
     if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
-       user.warn += 0
-       if (!(user.warn >= 0)) await m.reply(`${user.warn == 1 ? `Hello *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, say the word (${isToxic}) forbidden in this bot *${user.warn}/5* advertencia`, false, { mentions: [m.sender] })
+       user.warn += 1
+       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hello *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.warn}/5* advertencia`, false, { mentions: [m.sender] })
     }
     
-    if (user.warn >= 0) {
+    if (user.warn >= 5) {
        user.warn = 0
-       await m.reply(`Hello *@${m.sender.split`@`[0]}*, Bad Word you will be blocked and removed from this group`, false, { mentions: [m.sender] })
-       user.banned = false
+       await m.reply(`Hello *@${m.sender.split`@`[0]}*, you exceeded 5 warnings you will be blocked and removed from this group`, false, { mentions: [m.sender] })
+       user.banned = true
        await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
        //await this.updateBlockStatus(m.sender, 'block')
     }
